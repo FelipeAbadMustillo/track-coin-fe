546131ea2a975b15f04024c15e6e823ca2fa2cfa
Felipe Abad <felipeabadmustillo@gmail.com>
2024-03-20 21:11:58 -0300

    Trade Item
    This component is meant to show information about each trade individually, each of its characteristics in a register fashion.

--- track-coin/App.js
+++ track-coin/App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import TradeItem from './src/components/tradeItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Track Coin</Text>
       <StatusBar style="auto" />
+
+      <TradeItem />
     </View>
   );
 }
---
+++ track-coin/src/components/TradeItem.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import theme from "../theme.js"
+
+const TradeItem = (props) => (
+    <View>
+        <Text>Trade por 2 BitCoins</Text>
+    </View>
+)
+
+export default TradeItem
